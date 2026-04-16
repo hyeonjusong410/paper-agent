7a67b206faf43fd985cb38b629bcbc9b27d3147b
[hyunju410] <[20201735@daejin.ac.kr]>
2026-04-16 17:00:38 +0900

    fix max results

--- collector.py
+++ collector.py
@@ -20,11 +20,11 @@ def init_db():
     conn.commit()
     return conn
 
-def fetch_arxiv_papers(max_results=100):
+def fetch_arxiv_papers(max_results=300):
     client = arxiv.Client()
     search = arxiv.Search(
     query="cat:cs.AI OR cat:cs.LG OR cat:cs.CL",
-    max_results=100,  
+    max_results=300,  
     sort_by=arxiv.SortCriterion.SubmittedDate
 )
     papers = []
